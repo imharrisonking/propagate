a1a950959266d94739d22450bb118486e78ad07d
imharrisonking <harrison.k@hotmail.co.uk>
2024-01-22 09:23:59 +0000

    Fonts added and initial Layout component created

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+
+export default function Layout({ title, keywords, description, children }) {
+	return (
+		<div>
+			<Head>
+				<title>{title}</title>
+				<meta name="keywords" content={keywords} />
+				<meta name="description" content={description} />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main className="container mx-auto my-7">{children}</main>
+		</div>
+	);
+}
+
+Layout.defaultProps = {
+	title: 'Propagate | Digital Growth Agency',
+	keywords:
+		'propagate, propagate digital, propagate digital agency, agency, digital, growth, marketing, seo, social media, social media marketing, info products, ipga, digital products, digital marketing, digital agency, digital growth agency, digital growth',
+	description:
+		'Propagate is a digital growth agency specialising in helping experts build, launch and scale educational online businesses and products.',
+};
