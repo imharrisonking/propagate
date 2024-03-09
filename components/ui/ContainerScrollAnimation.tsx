606a0cb276e19f451f2bad2d55255b356b929f15
imharrisonking <harrison.k@hotmail.co.uk>
2024-03-09 11:11:41 +0000

    Reverse mobile mockup transition back to original transforms

--- components/ui/ContainerScrollAnimation.tsx
+++ components/ui/ContainerScrollAnimation.tsx
@@ -25,9 +25,9 @@ export const ContainerScrollAnimation = () => {
     return isMobile ? [0.7, 0.9] : [1.05, 1];
   };
 
-  const rotate = useTransform(scrollYProgress, [0, 1], [0, 20]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
   const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
-  const translate = useTransform(scrollYProgress, [0, 1], [-100, 0]);
+  const translate = useTransform(scrollYProgress, [0, 1], [0, -100]);
 
   return (
     <div
